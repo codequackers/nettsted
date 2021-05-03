acfaa56705b4eb5fe36269b6c10715d0aa875a0a
Frida Kvale <70896733+Fridask99@users.noreply.github.com>
2021-05-03 17:06:11 +0200

    Dropdown meny

--- HTML/Sidemeny/sidemeny.js
+++ HTML/Sidemeny/sidemeny.js
@@ -1,3 +1,27 @@
+window.onload = function(){
+    
+    
+    let dropdown = document.getElementsByClassName("dropdown-btn");
+    let size = dropdown.length;
+    console.log(dropdown[0]);
+    
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+    
+    
+    /*     console.log(dropdown.item(i));
+     */  /* dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      }); */
+    }
+}
+
 function show() {
 
     document.getElementById('sidebar').classList.toggle('active');
