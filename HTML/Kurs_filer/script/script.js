4acb64abfc7857bd99c7e722029274616552fbf9
espenraugstad <espen.raugstad@gmail.com>
2021-05-17 11:00:43 +0200

    Noen endringer, se spesielt 2.6

--- HTML/Kurs_filer/script/script.js
+++ HTML/Kurs_filer/script/script.js
@@ -2,15 +2,14 @@ window.onscroll = function(e){
     console.log(e.target.scrollingElement.scrollTop);
     if(e.target.scrollingElement.scrollTop > 0){
         // Add all classes relevant for scrolling
-        document.querySelector('.header').classList.add('scroll');
+        document.querySelector('header').classList.add('scroll');
         document.querySelector('.logo').classList.add('hidden');
 
 
     } else {
         // Remove classes
-        document.querySelector('.header').classList.remove('scroll');
+        document.querySelector('header').classList.remove('scroll');
         document.querySelector('.logo').classList.remove('hidden');
-
     }
     
 }
