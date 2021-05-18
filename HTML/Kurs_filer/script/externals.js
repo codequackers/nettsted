fbd079cfdbcde3d35737502e09b2a9ed2c5fe301
Frida Kvale <70896733+Fridask99@users.noreply.github.com>
2021-05-18 14:06:51 +0200

    heh

--- HTML/Kurs_filer/script/externals.js
+++ HTML/Kurs_filer/script/externals.js
@@ -23,16 +23,20 @@ const sidebar = `
     <h4>Innholdsfortegnelse</h4>
     <br>
     <br>
-    <a href="https://www.vg.no">Introduksjon til CodeQuackers læringsressurs</a>
 
 
     <button class="dropdown-btn">KAP.1 - Introduksjon til programmering
         <i class="fas fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Test 1</a>
-        <a href="#">Test 2</a>
-        <a href="#">Test 3</a>
+        <a href="../Kurs_filer/Introduksjon_av_oss.html">Introduksjon til CodeQuackers læringsressurs</a>
+        <a href="../Kurs_filer/Kurs_1_1.html">1.1 Læringsmål KAP 1</a>
+        <a href="../Kurs_filer/Kurs_1_2.html">1.2 Hva er programmering?</a>
+        <a href="../Kurs_filer/Kurs_1_3.html">1.3 Programmeringsspråk</a>
+        <a href="../Kurs_filer/Kurs_1_4.html">1.4 Hvor skriver vi koden?</a>
+        <a href="../Kurs_filer/Kurs_1_5.html">1.5 Hvor kjører vi koden?</a>
+        <a href="../Kurs_filer/Kurs_1_6.html">1.6 Programmering direkte i nettleseren</a>
+        <a href="../Kurs_filer/Kurs_1_7.html">1.7 Nettbaserte utviklingsmiljø</a>
     </div>
 
 
@@ -40,18 +44,26 @@ const sidebar = `
         <i class="fas fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Test 1</a>
-        <a href="#">Test 2</a>
-        <a href="#">Test 3</a>
+        <a href="../Kurs_filer/Kurs_2_1.html">2.1 Læringsmål KAP 2</a>
+        <a href="../Kurs_filer/Kurs_2_2.html">2.2 Dataprogram</a>
+        <a href="../Kurs_filer/Kurs_2_3.html">2.3 Hva er en variabel?</a>
+        <a href="../Kurs_filer/Kurs_2_4.html">2.4 Egenskapene til en variabel</a>
+        <a href="../Kurs_filer/Kurs_2_5.html">2.5 Lage variabler</a>
+        <a href="../Kurs_filer/Kurs_2_6.html">2.6 Navngiving av variabler</a>
     </div>
 
     <button class="dropdown-btn">KAP.3 - Datatyper, konstanter og operatorer
         <i class="fas fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Test 1</a>
-        <a href="#">Test 2</a>
-        <a href="#">Test 3</a>
+        <a href="../Kurs_filer/Kurs_3_1.html">3.1 Læringsmål KAP 3</a>
+        <a href="../Kurs_filer/Kurs_3_2.html">3.2 Datatyper</a>
+        <a href="../Kurs_filer/Kurs_3_3.html">3.3 Identifikatorer og literaler</a>
+        <a href="../Kurs_filer/Kurs_3_4.html">3.4 Hva er en konstant?</a>
+        <a href="../Kurs_filer/Kurs_3_5.html">3.5 Operatorer</a>
+        <a href="../Kurs_filer/Kurs_3_6.html">3.6 Aritmetiske operatorer</a>
+        <a href="../Kurs_filer/Kurs_3_7.html">3.7 Tilordningsoperatorer</a>
+        <a href="../Kurs_filer/Kurs_3_8.html">3.8 Modulus</a>
     </div>
 
 
@@ -59,9 +71,10 @@ const sidebar = `
         <i class="fas fa-caret-down"></i>
     </button>
     <div class="dropdown-container">
-        <a href="#">Test 1</a>
-        <a href="#">Test 2</a>
-        <a href="#">Test 3</a>
+        <a href="../Kurs_filer/Kurs_4_1.html">Læringsmål KAP 4</a>
+        <a href="../Kurs_filer/Kurs_4_2.html">If-setninger</a>
+        <a href="../Kurs_filer/Kurs_4_3.html">For-løkker</a>
+        <a href="../Kurs_filer/Kurs_4_4.html">While-løkker</a>
     </div>
 
 </div>
